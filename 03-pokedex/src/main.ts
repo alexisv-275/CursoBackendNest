dc8cd87c39703f3a5993860ae5e942eca9606d2c
alexisv-275 <alexis.villareal.lv@gmail.com>
2026-09-07 15:48:06 -0500

    DTO created with class validators

--- 03-pokedex/src/main.ts
+++ 03-pokedex/src/main.ts
@@ -1,10 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.setGlobalPrefix('api/v2'); // Establece un prefijo global para todas las rutas de la API
+  //Necesario para que los class validator funcionen ya que estos son los que interceptan la data
+  app.useGlobalPipes(  
+    new ValidationPipe({ 
+      //Solo deja la data que estoy esperando pero sigue recibiendo el mensaje
+      whitelist: true, 
+      //No permite recibir el mensaje si tiene data que no se espera
+      forbidNonWhitelisted: true, 
+    }) 
+  );
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
--- 03-pokedex/src/pokemon/dto/create-pokemon.dto.ts
+++ 03-pokedex/src/pokemon/dto/create-pokemon.dto.ts
@@ -1,1 +1,12 @@
-export class CreatePokemonDto {}
+import { IsNumber, IsPositive, IsString, Min, MinLength } from "class-validator";
+
+export class CreatePokemonDto {
+    @IsNumber()
+    @Min(1)
+    @IsPositive()
+    no: number;
+    
+    @IsString()
+    @MinLength(1)
+    name: string;
+}
