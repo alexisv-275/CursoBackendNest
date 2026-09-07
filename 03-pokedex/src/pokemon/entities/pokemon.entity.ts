735a3ea4f0fe8eb2d480e5abcfc91936ffd6fe6a
alexisv-275 <alexis.villareal.lv@gmail.com>
2026-09-07 15:22:42 -0500

    MongoDb connected to Nest

--- 03-pokedex/src/pokemon/entities/pokemon.entity.ts
+++ 03-pokedex/src/pokemon/entities/pokemon.entity.ts
@@ -1,1 +1,21 @@
-export class Pokemon {}
+import { Prop, Schema, SchemaFactory } from "@nestjs/mongoose";
+import { Document } from "mongoose";
+
+//Permite definir que es un esquema de base de datos
+@Schema()
+export class Pokemon extends Document{
+    //Definición de propiedades para que vayan así a la bd
+    @Prop({
+        unique:true, 
+        index: true
+    })
+    name: string;
+    @Prop({
+        unique:true, 
+        index: true
+    })
+    no: number;
+}
+
+//Convierte la clase Pokemon en un esquema que MongoDB puede entender
+export const PokemonSchema = SchemaFactory.createForClass(Pokemon);
--- 03-pokedex/src/pokemon/pokemon.module.ts
+++ 03-pokedex/src/pokemon/pokemon.module.ts
@@ -1,9 +1,22 @@
 import { Module } from '@nestjs/common';
 import { PokemonService } from './pokemon.service';
 import { PokemonController } from './pokemon.controller';
+import { Mongoose } from 'mongoose';
+// Conecta NestJS con MongoDB y permite registrar modelos de Mongoose.
+import { MongooseModule } from '@nestjs/mongoose';
+import { Pokemon, PokemonSchema } from './entities/pokemon.entity';
 
 @Module({
   controllers: [PokemonController],
   providers: [PokemonService],
+  imports:[
+    // Relaciona Pokemon con su esquema para acceder a la colección de Pokémon.
+    MongooseModule.forFeature([
+    {
+      name: Pokemon.name,
+      schema: PokemonSchema
+    }
+    ])
+  ]
 })
 export class PokemonModule {}
